e8fffff8efa075644e9ee6b375bc27a978108436
Pavel Ermakov <pavel@artisense.ai>
2020-07-31 13:01:57 +0200

    Lesson8 - K-Means clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -48,23 +48,35 @@ data_dict.pop("TOTAL", 0)
 ### can be any key in the person-level dictionary (salary, director_fees, etc.) 
 feature_1 = "salary"
 feature_2 = "exercised_stock_options"
+feature_3 = "total_payments"
 poi  = "poi"
-features_list = [poi, feature_1, feature_2]
+features_list = [poi, feature_1, feature_2, feature_3]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
+
+
+exercised_stock_options = [v["exercised_stock_options"] for v in data_dict.values() if v["exercised_stock_options"] != "NaN"]
+print(min(exercised_stock_options))
+print(max(exercised_stock_options))
+
+salary = [v["salary"] for v in data_dict.values() if v["salary"] != "NaN"]
+print(min(salary))
+print(max(salary))
 
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
 ### for f1, f2, _ in finance_features:
 ### (as it's currently written, the line below assumes 2 features)
-for f1, f2 in finance_features:
+for f1, f2, _ in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-
+from sklearn.cluster import KMeans
+kmeans = KMeans(n_clusters=2).fit(finance_features)
+pred = kmeans.predict(finance_features)
 
 
 
