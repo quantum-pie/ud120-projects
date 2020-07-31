1f43bd0c1c5b39d7d5c73d3f8b1efd3a32a795ad
Pavel Ermakov <pavel@artisense.ai>
2020-07-31 12:57:42 +0200

    Lesson5 - Exploring Enron Dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,32 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+print(len(enron_data))
+print(len(enron_data.values()[0]))
+
+interesting_stuff = {k: v for k, v in enron_data.items() if v["poi"] == 1}
+print(len(interesting_stuff))
+
+print(enron_data["PRENTICE JAMES"]["total_stock_value"])
+print(enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+print(enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+
+print(enron_data["SKILLING JEFFREY K"]["total_payments"])
+print(enron_data["LAY KENNETH L"]["total_payments"])
+print(enron_data["FASTOW ANDREW S"]["total_payments"])
+
+with_salary = {k: v for k, v in enron_data.items() if v["salary"] != 'NaN'}
+print(len(with_salary))
+
+with_email = {k: v for k, v in enron_data.items() if v["email_address"] != 'NaN'}
+print(len(with_email))
+
+no_total_payments = {k: v for k, v in enron_data.items() if v["total_payments"] == 'NaN'}
+print(len(no_total_payments))
+print(len(no_total_payments) / float(len(enron_data)))
+
+no_total_payments_interesting = {k: v for k, v in interesting_stuff.items() if v["total_payments"] == 'NaN'}
+print(len(no_total_payments_interesting))
+print(len(no_total_payments_interesting) / float(len(interesting_stuff)))
+
 
