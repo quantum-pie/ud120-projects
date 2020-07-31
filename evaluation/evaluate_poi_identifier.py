783ab24b12cce0142fdf06a06d4e6095bfcdf206
Pavel Ermakov <pavel@artisense.ai>
2020-07-31 13:11:17 +0200

    Lesson14 - Evaluation metrics

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -27,5 +27,33 @@ labels, features = targetFeatureSplit(data)
 
 
 ### your code goes here 
+### it's all yours from here forward!
+from sklearn import tree
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
+import numpy as np
 
+features_train, features_test, labels_train, labels_test = train_test_split(
+    features, labels, test_size=0.3, random_state=42)
+
+
+def classify(features_train, labels_train):
+    ### your code goes here!
+    clf = tree.DecisionTreeClassifier()
+    return clf.fit(features_train, labels_train)
+
+
+classifier = classify(features_train, labels_train)
+predicted_labels = classifier.predict(features_test)
+print("Detected POIs: " + str(sum(predicted_labels)))
+print("Total people: " + str(len(predicted_labels)))
+
+for true_label, pred_label in zip(labels_test, predicted_labels):
+    print("True label: " + str(true_label) + " Predicted label: " + str(pred_label))
+
+print(accuracy_score(labels_test, np.zeros(len(predicted_labels))))
+print(accuracy_score(labels_test, predicted_labels))
+
+print("Precision: " + str(precision_score(labels_test, predicted_labels)))
+print("Recall: " + str(recall_score(labels_test, predicted_labels)))
 
