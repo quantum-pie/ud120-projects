fff5761b958eebc3b3f36127709209c79c86087d
Pavel Ermakov <pavel@artisense.ai>
2020-07-31 11:41:05 +0200

    Lesson3 - Decision Tree classifier

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -13,18 +13,31 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+
+def classify(features_train, labels_train):
+    ### your code goes here!
+    clf = tree.DecisionTreeClassifier(min_samples_split=40)
+    return clf.fit(features_train, labels_train)
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+print(len(features_train[0]))
 
 #########################################################
 ### your code goes here ###
+t0 = time()
+classifier = classify(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+t0 = time()
+predicted_labels = classifier.predict(features_test)
+print "prediction time:", round(time()-t0, 3), "s"
+print("Accuracy: " + str(accuracy_score(labels_test, predicted_labels)))
 
 #########################################################
 
